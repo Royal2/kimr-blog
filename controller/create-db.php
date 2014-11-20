31a30be15a31e7698d20607d75f0f10672c75807
Roy Kim <1997.roy@gmail.com>
2014-11-20 15:00:44 -0800

    Checks our connection errors. Fixed our directory issue by using "__DIR__ ." to direct from current file.

--- controller/create-db.php
+++ controller/create-db.php
@@ -1,6 +1,17 @@
 <?php
     //Brings the data from database.php to create-db.php
     //require once also checks whether it has already been inserted into memory.
-    require_once("../model/database.php");
+    require_once(__DIR__ ."/../model/database.php");
     //$connection carries the functions to access database server.
     $connection = new mysqli($host, $username, $password);
+    //a conditional statement that checks whether there is an error connecting to the server.
+    if($connection->connect_error) {
+        //if there is an error connecting to the server: stops the code and prints an error.
+        die("Error: " . $connection->connect_error);
+    }
+    else {
+        //if there is no error connecting to the server: prints success.
+        echo "Success" . $connection->host_info;
+    }
+    //closes the connection.
+    $connection->close();
--- index.php
+++ index.php
@@ -6,7 +6,9 @@
     </head>
     <body>
         <?php
-            echo "Web server is running locally.";
+            //"__DIR__ ." gets the directory in which the current file (index.php) is in, and from there it follow the path. 
+            require_once(__DIR__ ."/controller/create-db.php");
+            
         ?>
     </body>
 </html>
