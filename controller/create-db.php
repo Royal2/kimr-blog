30136418df433a4bbb31583e51fad2b897cb7db1
Roy Kim <1997.roy@gmail.com>
2014-12-02 15:00:04 -0800

    Created table posts and columns. Checked if table posts was created.

--- controller/create-db.php
+++ controller/create-db.php
@@ -7,7 +7,7 @@
     //a conditional statement that checks whether there is an error connecting to the server.
     if($connection->connect_error) {
         //if there is an error connecting to the server: stops the code and prints an error.
-        die("Error: " . $connection->connect_error);
+        die("<p>Error: " . $connection->connect_error . "</p>");
     }
     //selects Database.
     $exists = $connection->select_db($database);
@@ -17,12 +17,27 @@
         $query = $connection->query("CREATE DATABASE $database");
         //checks if database was created.
         if($query){
-            echo "Successfully created database.".$database;
+            echo "<p>Successfully created database.".$database . "</p>";
         }
     }
     else{
         //echoes if database exists.
         echo "Database already exists.";
+    }
+    //sends query to create a table named posts.
+    $query = $connection->query("CREATE TABLE posts ("
+            . "id int(11) NOT NULL AUTO_INCREMENT,"    //creates column "id".
+            . "title varchar(255) NOT NULL," //creates column "title".
+            . "post text NOT NULL,"    //creates column "post".
+            . "PRIMARY KEY (id))"); //set primary key to id, primary key is a unique character.
+    //checks if table posts was created.
+    if($query){
+        //echos if table posts was created.
+        echo "<p>Successfully created table: posts<p>";
+    }
+    else{
+        //echos error if table posts was not created or already exists.
+        echo "<p>$connection->error</p>";
     }
     
     //closes the connection.
