976a3a1129e0fcc899b58b6508f4a59113742cf5
Roy Kim <1997.roy@gmail.com>
2014-12-03 14:54:40 -0800

    Created a header and footer as individual files, this enables us to use the same header and footer for multiple web pages.

--- index.php
+++ index.php
@@ -1,14 +1,8 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-            //"__DIR__ ." gets the directory in which the current file (index.php) is in, and from there it follow the path. 
-            require_once(__DIR__ ."/controller/create-db.php");
-            
-        ?>
-    </body>
-</html>
+<?php
+    //Inserts all the code in header.php into index.php
+    require_once(__DIR__ ."/view/header.php");
+    //"__DIR__ ." gets the directory in which the current file (index.php) is in, and from there it follow the path. 
+    require_once(__DIR__ ."/controller/create-db.php");
+    //Inserts all the code in footer.php into index.php
+    require_once(__DIR__ ."/view/footer.php");
+?>
