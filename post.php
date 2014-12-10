0a63314e603e21ae95c6221110ed51828841e361
Roy Kim <1997.roy@gmail.com>
2014-12-09 23:25:40 -0800

    Created a blog post form. Created a submit button for blog post form.

--- post.php
+++ post.php
@@ -1,8 +1,7 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    //Inserts header.
+    require_once(__DIR__ . "/view/header.php");
+    //Inserts form.
+    require_once(__DIR__ . "/view/form.php");
+    //Inserts footer.
+    require_once(__DIR__ . "/view/footer.php");
---
+++ view/form.php
@@ -0,0 +1,17 @@
+<h1>Create Blog post</h1>   <!-- form header -->
+<!-- Creating Blog post form -->
+<form>
+    <div>
+        <label for="title">Title: </label>
+        <input type="text" name="title"/>   <!-- input type text allows for one line of text -->
+    </div>
+    
+    <div>
+        <label for="post">Post: </label>
+        <textarea name="post"></textarea>   <!-- textarea allows multiple lines of text -->
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>   <!-- Creates a submit button for the form -->
+    </div>
+</form>
