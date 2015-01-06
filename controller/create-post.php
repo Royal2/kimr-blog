324fb5abfa3a4d58a36e04f0a4e11e08ff2574ae
Roy Kim <1997.roy@gmail.com>
2015-01-06 15:03:13 -0800

    Created controller to receive blog posts. Connected controller to form. Established a db connection. Created a query to send blog post to db.

---
+++ controller/create-post.php
@@ -0,0 +1,21 @@
+<?php
+    require_once(__DIR__ . "/../model/database.php");
+    //creates a new connection.
+    $connection = new mysqli($host, $username, $password, $database);
+    
+    //recieves form post and sets it to a variable.
+    $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+    $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING);
+    //echoes form post.
+    echo "<p>Title: $title</p>";
+    echo "<p>Post: $post</p>";
+    //creates query to insert into db.
+    $query = $connection->query("INSERT INTO posts SET title = '$title', post = '$post'");
+    //checks query.
+    if($query){
+        echo "<p>Successfully inserted post: $title</p>";
+    }
+    else {
+        echo "<p>$connection->error</p>";
+    }
+    $connection->close();
--- view/form.php
+++ view/form.php
@@ -1,6 +1,10 @@
+<?php
+    require_once(__DIR__ . "/../model/config.php");
+?>
+
 <h1>Create Blog post</h1>   <!-- form header -->
 <!-- Creating Blog post form -->
-<form>
+<form method="post" action="<?php echo $path . "controller/create-post.php"; ?>">    <!-- specifies form information type and where to send the form information -->
     <div>
         <label for="title">Title: </label>
         <input type="text" name="title"/>   <!-- input type text allows for one line of text -->
