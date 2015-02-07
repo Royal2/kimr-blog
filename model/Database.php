206cab39d3af53878ccc93e1bcb60b7d91fa194a
Roy Kim <1997.roy@gmail.com>
2015-02-06 23:34:26 -0800

    Created a constructor. Created functions within our database class. Implemented openConnection function. Implemented closeConnection function. Implemented the query function.

--- model/Database.php
+++ model/Database.php
@@ -5,6 +5,41 @@
         private $username;
         private $password;
         private $database;
+        //Constructor class.
+        public function __construct($host, $username, $password, $database) {
+            //Storing information.
+            $this->host = $host;
+            $this->username = $username;
+            $this->password = $password;
+            $this->database = $database;
+        }
+        
+        public function openConnection() {
+            //creating new connection.
+            $this->connection = new mysqli($this->host, $this->username,$this->password, $this->database);
+            //checks for connection errors.
+            if($this->connection->connect_error) {
+                die("<p>Error: " . $this->connection->connection_error . "<p>");
+            }
+        }
+        
+        public function closeConnection() {
+            //closes connection if it is set.
+            if(isset($this->connection)) {
+                $this->connection->close();
+            }
+        }
+        
+        public function query($string) {
+            $this->openConnection();
+
+            $query = $this->connection->query($string);
+
+            $this->closeConnection();
+
+            return $query;
+        }
+            
     }
     
     
